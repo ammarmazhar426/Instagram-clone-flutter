aa445c8152f4d056e3940d4114c5704b46ff125d
Ammar Mazhar <109351247+ammarmazhar426@users.noreply.github.com>
2024-11-16 11:25:43 +0500

    Update bg-col main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           elevation: 1,
-          color: Colors.white,
+          color: Colors.black,
           iconTheme: IconThemeData(color: Colors.black)
         ),
         primarySwatch: Colors.blue,
