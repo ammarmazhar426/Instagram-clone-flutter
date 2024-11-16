0d857cec69ec663317acb8a50024779cc011d47d
Ammar Mazhar <109351247+ammarmazhar426@users.noreply.github.com>
2024-11-16 18:30:17 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           elevation: 1,
-          color: Colorswhite,
+          color: Colors.white,
           iconTheme: IconThemeData(color: Colors.black)
         ),
         primarySwatch: Colors.blue,
